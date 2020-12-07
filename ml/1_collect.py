70113b50dfad20be753717d9175919e0fe2c7d40
kangrnin <kangrnin@gmail.com>
2020-12-08 00:19:09 +0900

    Merge wifi_scan and collect

--- ml/1_collect.py
+++ ml/1_collect.py
@@ -1,54 +1,71 @@
-from pathlib import Path
+import subprocess
+import time
+import datetime
 import pandas as pd
-from wifi_scan import get_wifis, enable_wifi
+from pathlib import Path
+
+def get_wifis():
+    subprocess.run(
+        ['netsh', 'interface', 'set', 'interface', 'name="Wi-Fi"', 'admin=disabled'],
+        capture_output=True)
+    subprocess.run(['netsh', 'interface', 'set', 'interface', 'name="Wi-Fi"', 'admin=enabled'],
+        capture_output=True)
+
+    time.sleep(1)
+    output = subprocess.run(
+        ['netsh', 'wlan', 'show', 'network', 'mode=Bssid'],
+        capture_output=True, text=True, encoding='ISO-8859-1').stdout
+
+    results = output.split('\n\n')[1:-1]
+
+    timestamp = datetime.datetime.now()
+    wifis = []
+    for result in results:
+        lines = result.split('\n')
+
+        for i in range(len(lines)):
+            if lines[i].split()[0] == 'BSSID':
+                bssid = lines[i].split()[-1]
+                signal = lines[i+1].split()[-1][:-1]
+                wifis.append({'bssid':bssid, 'signal':signal, 'timestamp':timestamp})
+
+    return wifis
 
 def collect(position, rp):
-    #position = str(position)
-
     script_path = Path(__file__).parent
-    data_path = script_path / '../signal_data'
-    # 데이터 파일 폴더 생성
+    data_path = script_path / '../wifi_data' / position
     data_path.mkdir(parents=True, exist_ok=True)
-    (data_path/position).mkdir(parents=True, exist_ok=True)
 
-    wifi_list = []
+    wifi_list = list()
     bssid_set = set()
-
     overlap_cnt = 0
-    scan_cnt = 1
-    while True:
-        print('-----------------------------------------')
+    scan_cnt = 0
 
+    while overlap_cnt < 3:
         scan_wifis = get_wifis()
-        print('total bssid cnt : ', len(scan_wifis))
 
-        new_bssids = [ w['bssid'] for w in scan_wifis if w['bssid'] not in bssid_set ]
-        bssid_set.update(new_bssids)
-
-        print(len(new_bssids), 'new bssids : [ ', end='')
-        for bssid in new_bssids:
-            print('~'+bssid[-8:], end=', ')
-        print(' ]')
-
-        overlap_cnt = overlap_cnt+1 if len(new_bssids) == 0 else 0
-        
-        if overlap_cnt == 3:
-            break
-
-        if new_bssids:
+        new_bssid = [ w['bssid'] for w in scan_wifis if w['bssid'] not in bssid_set ]
+        bssid_set.update(new_bssid)
+        if new_bssid:
             wifi_list += scan_wifis
+        else:
+            overlap_cnt += 1
 
-        print('completed scan #', scan_cnt)
         scan_cnt += 1
-    
-    # (position) 폴더 안의 (rp).csv 안에 저장
-    df = pd.DataFrame(wifi_list)
-    df.to_csv(data_path/position/(rp+'.csv'), mode='a', index=False, header=False)
 
-    # position, rp 정보 column을 추가해서 통합 데이터파일에도 저장
+        print('-----------------------------------------')
+        print(f'scan #{ scan_cnt } total bssid cnt : {len(scan_wifis)}')
+        print(f'{ len(new_bssid) } new bssids : ')
+        print(', '.join([f'~{ b[-8:] }' for b in new_bssid]))
+
+    print('scan complete. bssid cnt : '+str(len(bssid_set)))
+
+    df = pd.DataFrame(wifi_list)
     df['position'] = position
     df['rp'] = rp
-    df.to_csv(data_path/position/'signal_all.csv', mode='a', index=False, header=False)
+
+    df.to_csv(data_path/(f'{ rp }.csv'), mode='a', index=False, header=False)
+    df.to_csv(data_path/'wifi_all.csv', mode='a', index=False, header=False)
 
 if __name__ == "__main__":
     while True:
